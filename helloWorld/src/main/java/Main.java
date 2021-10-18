9545e1de06136e57d4347a69d2a003e6764eee5e
Rashid-F-Walcott <rashidwalcott@gmail.com>
2021-10-18 12:43:35 +0100

    Data types intro on Day1

--- helloWorld/src/main/java/Main.java
+++ helloWorld/src/main/java/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("jello world");
+        String name = "Rashid";
+
+        int age = 20;
+        long age2 = 20L;
+
+        double pi = 3.14;
+        float pi2 = 3.14F;
+
+        char letter = 'A';
+
+
+
     }
 }
